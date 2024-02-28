f83ee12fc1ef1cff446bed6a3390b069941a848d
Leonhard Markert <curiousleo@users.noreply.github.com>
2024-02-28 18:59:27 +0800

    Changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11-slim-bookworm
+FROM docker.io/library/python:3.11-slim-bookworm
 
 RUN set -eux; \
 	apt-get update; \
--- export-all-formats.sh
+++ export-all-formats.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuox pipefail
 
-# axel --num-connections=10 --quiet \
-#     --output=/mnt/out/Gesamtdatenexport.zip \
-#     "$(python -m mastr-export print-export-url)"
-# python -m mastr-export extract \
-#     /mnt/out/Gesamtdatenexport.zip \
-#     --parquet-dir /mnt/out/parquet \
-#     --csv-dir /mnt/out/csv
+axel --num-connections=10 --quiet \
+    --output=/mnt/out/Gesamtdatenexport.zip \
+    "$(python -m mastr-export print-export-url)"
+python -m mastr-export extract \
+    /mnt/out/Gesamtdatenexport.zip \
+    --parquet-dir /mnt/out/parquet \
+    --csv-dir /mnt/out/csv
 
 pushd /mnt/out/parquet
 duckdb '/mnt/out/bnetza.duckdb' -init 'import-duckdb.sql' -bail -batch -echo -no-stdin
