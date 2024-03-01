0ca6a08d60d75a08cecacaf580c25ddd2c31d10a
Leonhard Markert <curiousleo@users.noreply.github.com>
2024-03-01 20:25:44 +0800

    Use new command line arguments

--- export-all-formats.sh
+++ export-all-formats.sh
@@ -6,13 +6,7 @@ axel --num-connections=10 --quiet \
     "$(python -m mastr-export print-export-url)"
 python -m mastr-export extract \
     /mnt/out/Gesamtdatenexport.zip \
+    --duckdb /mnt/out/bnetza.duckdb \
+    --sqlite /mnt/out/bnetza.sqlite \
     --parquet-dir /mnt/out/parquet \
     --csv-dir /mnt/out/csv
-
-pushd /mnt/out/parquet
-duckdb '/mnt/out/bnetza.duckdb' -init 'import-duckdb.sql' -bail -batch -echo -no-stdin
-popd
-
-pushd /mnt/out/csv
-sqlite3 -bail '/mnt/out/bnetza.sqlite3' <'import-sqlite.sql'
-popd
