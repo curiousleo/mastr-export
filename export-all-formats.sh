25f8eb810bcc1f855198d4f336534d0c717578fd
Leonhard Markert <curiousleo@users.noreply.github.com>
2024-03-01 21:07:30 +0800

    Move arguments around

--- export-all-formats.sh
+++ export-all-formats.sh
@@ -5,8 +5,8 @@ axel --num-connections=10 --quiet \
     --output=/mnt/out/Gesamtdatenexport.zip \
     "$(python -m mastr-export print-export-url)"
 python -m mastr-export extract \
-    /mnt/out/Gesamtdatenexport.zip \
     --duckdb /mnt/out/bnetza.duckdb \
     --sqlite /mnt/out/bnetza.sqlite \
     --parquet-dir /mnt/out/parquet \
-    --csv-dir /mnt/out/csv
+    --csv-dir /mnt/out/csv \
+    /mnt/out/Gesamtdatenexport.zip
