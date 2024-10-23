0d3db0a24f75acc22f8a6a2bc28ff2385fe8a375
Leonhard Markert <curiousleo@users.noreply.github.com>
2024-10-23 22:11:10 +0800

    Remove deployment stuff

--- download-and-extract-sqlite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuox pipefail
-
-axel --num-connections=10 --quiet \
-    --output=/mnt/out/Gesamtdatenexport.zip \
-    "$(python -m mastr-export print-export-url)"
-python -m mastr-export extract-to-sqlite \
-    --export /mnt/out/Gesamtdatenexport.zip \
-    --sqlite /mnt/out/bnetza.sqlite3
