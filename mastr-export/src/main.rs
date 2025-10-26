9b4917fd711bdf8f661982e6341b0ec32fa4e708
Leonhard Markert <curiousleo@users.noreply.github.com>
2025-10-26 18:31:48 +0800

    Schema

--- mastr-export/src/main.rs
+++ mastr-export/src/main.rs
@@ -1,3 +1,4 @@
+use arrow::datatypes::{DataType, TimeUnit};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::str::from_utf8;
@@ -5,117 +6,149 @@ use std::str::from_utf8;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
 
-#[derive(Serialize, Deserialize)]
-struct Reference<'a> {
-    table: &'a str,
-    column: &'a str,
+#[derive(Debug, Serialize, Deserialize)]
+struct Reference {
+    table: String,
+    column: String,
 }
 
-fn to_sqlite_schema(reference: &Reference) -> String {
-    format!(
-        "references \"{}\"(\"{}\")",
-        reference.table, reference.column
-    )
-}
+// fn to_sqlite_schema(reference: &Reference) -> String {
+//     format!(
+//         "references \"{}\"(\"{}\")",
+//         reference.table, reference.column
+//     )
+// }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 enum XsdType {
+    #[serde(rename = "date")]
     Date,
+    #[serde(rename = "dateTime")]
     DateTime,
+    #[serde(rename = "float")]
     Float,
+    #[serde(rename = "double")]
     Double,
-    Decimal,
+    #[serde(rename = "byte")]
     Byte,
+    #[serde(rename = "short")]
     Short,
+    #[serde(rename = "int")]
     Int,
+    #[serde(rename = "nonNegativeInteger")]
     NonNegativeInteger,
+    #[serde(rename = "boolean")]
     Boolean,
+    #[serde(rename = "string")]
     String,
 }
 
-enum DuckDBType {
-    Date,
-    Timestamp,
-    Float,
-    Double,
-    Tinyint,
-    Smallint,
-    Integer,
-    Ubigint,
-    Boolean,
-    Text,
-}
-
-fn to_duckdb_type(xsdType: &XsdType) -> DuckDBType {
-    match xsdType {
-        XsdType::Date => DuckDBType::Date,
-        XsdType::DateTime => DuckDBType::Timestamp,
-        XsdType::Float => DuckDBType::Float,
-        XsdType::Double => DuckDBType::Double,
-        XsdType::Decimal => DuckDBType::Double,
-        XsdType::Byte => DuckDBType::Tinyint,
-        XsdType::Short => DuckDBType::Smallint,
-        XsdType::Int => DuckDBType::Integer,
-        XsdType::NonNegativeInteger => DuckDBType::Ubigint,
-        XsdType::Boolean => DuckDBType::Boolean,
-        XsdType::String => DuckDBType::Text,
+impl Default for XsdType {
+    fn default() -> Self {
+        XsdType::String
     }
 }
 
-fn duckdb_type_to_string(duckDBType: DuckDBType) -> &'static str {
-    match duckDBType {
-        DuckDBType::Date => "date",
-        DuckDBType::Timestamp => "timestamp",
-        DuckDBType::Float => "float",
-        DuckDBType::Double => "double",
-        DuckDBType::Tinyint => "tinyint",
-        DuckDBType::Smallint => "smallint",
-        DuckDBType::Integer => "integer",
-        DuckDBType::Ubigint => "ubigint",
-        DuckDBType::Boolean => "boolean",
-        DuckDBType::Text => "text",
+impl Into<DataType> for &XsdType {
+    fn into(self) -> DataType {
+        match self {
+            XsdType::Date => DataType::Date32,
+            XsdType::DateTime => DataType::Timestamp(TimeUnit::Second, None),
+            XsdType::Float => DataType::Float32,
+            XsdType::Double => DataType::Float64,
+            XsdType::Byte => DataType::Int8,
+            XsdType::Short => DataType::Int16,
+            XsdType::Int => DataType::Int32,
+            XsdType::NonNegativeInteger => DataType::UInt64,
+            XsdType::Boolean => DataType::Boolean,
+            XsdType::String => DataType::Utf8,
+        }
     }
 }
 
-#[derive(Serialize, Deserialize)]
-struct Field<'a> {
-    name: &'a str,
+#[derive(Debug, Serialize, Deserialize)]
+struct Field {
+    name: String,
+    #[serde(default)]
     index: bool,
+    #[serde(default)]
     xsd: XsdType,
-    references: Option<Reference<'a>>,
+    #[serde(default)]
+    references: Option<Reference>,
 }
 
-fn to_duckdb_schema(field: &Field) -> String {
-    let duckdb_type = duckdb_type_to_string(to_duckdb_type(&field.xsd));
-    format!("{} {}", field.name, duckdb_type)
+impl Into<arrow::datatypes::Field> for &Field {
+    fn into(self) -> arrow::datatypes::Field {
+        let data_type = Into::<arrow::datatypes::DataType>::into(&self.xsd);
+        let mut field = arrow::datatypes::Field::new(&self.name, data_type, true);
+        if self.index {
+            field = field.with_metadata(HashMap::from([("index".to_string(), "true".to_string())]));
+        }
+        if let Some(Reference { table, column }) = &self.references {
+            field = field.with_metadata(HashMap::from([
+                ("references_table".to_string(), table.to_string()),
+                ("references_column".to_string(), column.to_string()),
+            ]));
+        }
+        field
+    }
 }
 
-#[derive(Serialize, Deserialize)]
-struct Spec<'a> {
-    root: &'a str,
-    element: &'a str,
+// fn to_duckdb_schema(field: &Field) -> String {
+//     let duckdb_type = duckdb_type_to_string(to_duckdb_type(&field.xsd));
+//     format!("{} {}", field.name, duckdb_type)
+// }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Schema {
+    root: String,
+    element: String,
+    #[serde(default)]
     without_rowid: bool,
-    primary: Option<&'a str>,
-    fields: HashMap<&'a str, Field<'a>>,
+    #[serde(default)]
+    primary: Option<String>,
+    fields: Vec<Field>,
 }
 
-fn duckdb_schema<'a>(spec: Spec<'a>) -> String {
-    let columns = spec
-        .fields
-        .values()
-        .map(|field| to_duckdb_schema(field))
-        .collect::<Vec<_>>()
-        .join(",\n    ");
-    let primary = if let Some(primary) = spec.primary {
-        &format!(",\nprimary key (\"{}\")", primary)
-    } else {
-        ""
-    };
-    format!(
-        "create table if not exists \"{}\" (\n{}{}))",
-        spec.element, columns, primary
-    )
+impl Into<arrow::datatypes::Schema> for &Schema {
+    fn into(self) -> arrow::datatypes::Schema {
+        let fields = self
+            .fields
+            .iter()
+            .map(|field| Into::<arrow::datatypes::Field>::into(field))
+            .collect::<Vec<_>>();
+        let fields = arrow::datatypes::Fields::from(fields);
+        let mut metadata = HashMap::from([
+            ("root".to_string(), self.root.to_string()),
+            ("element".to_string(), self.element.to_string()),
+        ]);
+        if self.without_rowid {
+            metadata.insert("without_rowid".to_string(), "true".to_string());
+        }
+        if let Some(primary) = &self.primary {
+            metadata.insert("primary".to_string(), primary.to_string());
+        }
+        arrow::datatypes::Schema::new(fields).with_metadata(metadata)
+    }
 }
+
+// fn duckdb_schema<'a>(spec: Schema<'a>) -> String {
+//     let columns = spec
+//         .fields
+//         .values()
+//         .map(|field| to_duckdb_schema(field))
+//         .collect::<Vec<_>>()
+//         .join(",\n    ");
+//     let primary = if let Some(primary) = spec.primary {
+//         &format!(",\nprimary key (\"{}\")", primary)
+//     } else {
+//         ""
+//     };
+//     format!(
+//         "create table if not exists \"{}\" (\n{}{}))",
+//         spec.element, columns, primary
+//     )
+// }
 
 #[derive(Debug)]
 enum ParserState {
@@ -126,7 +159,7 @@ enum ParserState {
     Done,
 }
 
-fn parse<'a>(spec: Spec<'a>, mut reader: Reader<&[u8]>) -> HashMap<String, Vec<String>> {
+fn parse<'a>(spec: &Schema, mut reader: Reader<&[u8]>) -> HashMap<String, Vec<String>> {
     let mut columns: HashMap<String, Vec<String>> = HashMap::new();
     let mut state = ParserState::StartRoot;
     let mut buf = Vec::new();
@@ -219,5 +252,8 @@ fn parse<'a>(spec: Spec<'a>, mut reader: Reader<&[u8]>) -> HashMap<String, Vec<S
 }
 
 fn main() {
-    println!("Hello, world!");
+    let path = std::path::PathBuf::from(std::env::args().nth(1).unwrap());
+    let file = std::fs::File::open(path).unwrap();
+    let schema: Schema = serde_json::from_reader(file).unwrap();
+    println!("{:?}", schema);
 }
