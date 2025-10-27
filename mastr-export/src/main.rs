b62f61cc5647cd434c6515e74f4a9ed04664bef8
Leonhard Markert <curiousleo@users.noreply.github.com>
2025-10-27 21:16:45 +0800

    Use xml package, quick-xml does not support UTF-16

--- mastr-export/src/main.rs
+++ mastr-export/src/main.rs
@@ -1,10 +1,11 @@
 use arrow::datatypes::{DataType, TimeUnit};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::str::from_utf8;
 
-use quick_xml::events::Event;
-use quick_xml::reader::Reader;
+use xml::{
+    name::OwnedName,
+    reader::{EventReader, ParserConfig, XmlEvent},
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Reference {
@@ -70,27 +71,13 @@ impl Into<DataType> for &XsdType {
 struct Field {
     name: String,
     #[serde(default)]
-    index: bool,
-    #[serde(default)]
     xsd: XsdType,
-    #[serde(default)]
-    references: Option<Reference>,
 }
 
 impl Into<arrow::datatypes::Field> for &Field {
     fn into(self) -> arrow::datatypes::Field {
         let data_type = Into::<arrow::datatypes::DataType>::into(&self.xsd);
-        let mut field = arrow::datatypes::Field::new(&self.name, data_type, true);
-        if self.index {
-            field = field.with_metadata(HashMap::from([("index".to_string(), "true".to_string())]));
-        }
-        if let Some(Reference { table, column }) = &self.references {
-            field = field.with_metadata(HashMap::from([
-                ("references_table".to_string(), table.to_string()),
-                ("references_column".to_string(), column.to_string()),
-            ]));
-        }
-        field
+        arrow::datatypes::Field::new(&self.name, data_type, true)
     }
 }
 
@@ -152,6 +139,7 @@ impl Into<arrow::datatypes::Schema> for &Schema {
 
 #[derive(Debug)]
 enum ParserState {
+    StartDocument,
     StartRoot,
     StartElementOrEndRoot,
     StartAttrOrEndElement,
@@ -159,86 +147,101 @@ enum ParserState {
     Done,
 }
 
-fn parse<'a>(spec: &Schema, mut reader: Reader<&[u8]>) -> HashMap<String, Vec<String>> {
+// fn parse<R>(_spec: &Schema, reader: EventReader<R>)
+// where
+//     R: std::io::BufRead,
+// {
+//     for event in reader {
+//         match event {
+//             Err(e) => panic!("Error: {:?}", e),
+//             Ok(event) => {
+//                 println!("{:?}", event);
+//             }
+//         }
+//     }
+// }
+
+fn parse<R>(spec: &Schema, reader: EventReader<R>) -> HashMap<String, Vec<String>>
+where
+    R: std::io::BufRead,
+{
     let mut columns: HashMap<String, Vec<String>> = HashMap::new();
-    let mut state = ParserState::StartRoot;
-    let mut buf = Vec::new();
-    loop {
-        match (&state, reader.read_event_into(&mut buf)) {
-            (state, Err(e)) => panic!(
-                "Error in state {:?} at position {}: {:?}",
-                state,
-                reader.error_position(),
-                e
-            ),
-            (ParserState::StartRoot, Ok(Event::Start(e))) => {
-                if spec.root.as_bytes() == e.name().as_ref() {
+    let mut state = ParserState::StartDocument;
+    for event in reader {
+        match (&mut state, event) {
+            (ParserState::StartDocument, Ok(XmlEvent::StartDocument { .. })) => {
+                state = ParserState::StartRoot;
+            }
+            (state, Err(e)) => panic!("Error in state {:?}: {:?}", state, e),
+            (ParserState::StartRoot, Ok(XmlEvent::Characters(_))) => {
+                // Leading text, ignore
+            }
+            (ParserState::StartRoot, Ok(XmlEvent::StartElement { name, .. })) => {
+                let OwnedName { local_name, .. } = name;
+                if spec.root == local_name {
                     state = ParserState::StartElementOrEndRoot;
                 } else {
-                    panic!(
-                        "Expected root element {}, got {:?}",
-                        spec.root,
-                        e.name().as_ref()
-                    )
+                    panic!("Expected root element {}, got {:?}", spec.root, local_name)
                 }
             }
-            (ParserState::StartElementOrEndRoot, Ok(Event::Start(e))) => {
-                let name = e.name();
-                let name = from_utf8(name.as_ref()).unwrap();
-                if spec.element != name {
-                    panic!("Unknown element {}", name)
+            (ParserState::StartElementOrEndRoot, Ok(XmlEvent::StartElement { name, .. })) => {
+                let OwnedName { local_name, .. } = name;
+                if spec.element != local_name {
+                    panic!("Unknown element {}", local_name)
                 }
                 columns.values_mut().for_each(|v| v.push("".to_string()));
                 state = ParserState::StartAttrOrEndElement
             }
-            (ParserState::StartAttrOrEndElement, Ok(Event::Start(e))) => {
-                let name = e.name();
-                let name = from_utf8(name.as_ref()).unwrap();
-                if !columns.contains_key(name) {
-                    panic!("Unknown attribute {}", name)
+            (ParserState::StartAttrOrEndElement, Ok(XmlEvent::StartElement { name, .. })) => {
+                let OwnedName { local_name, .. } = name;
+                if !columns.contains_key(&local_name) {
+                    println!("Unknown field {}", local_name)
                 }
-                state = ParserState::AttrCdataOrEndAttr(name.to_string())
+                state = ParserState::AttrCdataOrEndAttr(local_name)
             }
-            (ParserState::AttrCdataOrEndAttr(element), Ok(Event::Text(e))) => {
-                let content = e.xml_content().unwrap();
-                columns
-                    .get_mut(element)
-                    .unwrap()
-                    .last_mut()
-                    .unwrap()
-                    .push_str(content.as_ref());
+            (ParserState::AttrCdataOrEndAttr(element), Ok(XmlEvent::Characters(content))) => {
+                match columns.get_mut(element) {
+                    Some(column) => {
+                        column.last_mut().unwrap().push_str(content.as_ref());
+                    }
+                    None => {
+                        // Ignore
+                    }
+                }
             }
-            (ParserState::AttrCdataOrEndAttr(element), Ok(Event::End(e))) => {
-                let name = e.name();
-                let name = from_utf8(name.as_ref()).unwrap();
-                if name != element {
-                    panic!("Expected closing of tag {}, got {}", element, name)
+            (ParserState::AttrCdataOrEndAttr(element), Ok(XmlEvent::EndElement { name })) => {
+                let OwnedName { local_name, .. } = name;
+                if &local_name != element {
+                    panic!("Expected closing of tag {}, got {}", element, local_name)
                 }
                 state = ParserState::StartAttrOrEndElement
             }
-            (ParserState::StartAttrOrEndElement, Ok(Event::End(e))) => {
-                let name = e.name();
-                let name = from_utf8(name.as_ref()).unwrap();
-                if spec.element != name {
-                    panic!("Expected closing of tag {}, got {}", spec.element, name)
+            (ParserState::StartAttrOrEndElement, Ok(XmlEvent::EndElement { name })) => {
+                let OwnedName { local_name, .. } = name;
+                if spec.element != local_name {
+                    panic!(
+                        "Expected closing of tag {}, got {}",
+                        spec.element, local_name
+                    )
                 }
                 state = ParserState::StartElementOrEndRoot
             }
-            (ParserState::StartElementOrEndRoot, Ok(Event::End(e))) => {
-                let name = e.name();
-                let name = from_utf8(name.as_ref()).unwrap();
-                if spec.root != name {
-                    panic!("Expected closing of tag {}, got {}", spec.element, name)
+            (ParserState::StartElementOrEndRoot, Ok(XmlEvent::EndElement { name })) => {
+                let OwnedName { local_name, .. } = name;
+                if spec.root != local_name {
+                    panic!(
+                        "Expected closing of tag {}, got {}",
+                        spec.element, local_name
+                    )
                 }
                 state = ParserState::Done
             }
-            (ParserState::Done, Ok(Event::Eof)) => break,
             (
                 _,
-                Ok(Event::Comment(_))
-                | Ok(Event::PI(_))
-                | Ok(Event::DocType(_))
-                | Ok(Event::Empty(_)),
+                Ok(XmlEvent::Comment(_))
+                | Ok(XmlEvent::CData(_))
+                | Ok(XmlEvent::Doctype { .. })
+                | Ok(XmlEvent::ProcessingInstruction { .. }),
             ) => {
                 // Ignore
             }
@@ -246,14 +249,31 @@ fn parse<'a>(spec: &Schema, mut reader: Reader<&[u8]>) -> HashMap<String, Vec<St
                 panic!("Unexpected event {:?} in state {:?}", state, event)
             }
         }
-        buf.clear();
     }
     columns
 }
 
 fn main() {
-    let path = std::path::PathBuf::from(std::env::args().nth(1).unwrap());
-    let file = std::fs::File::open(path).unwrap();
-    let schema: Schema = serde_json::from_reader(file).unwrap();
-    println!("{:?}", schema);
+    let schema_path = std::path::PathBuf::from(std::env::args().nth(1).unwrap());
+    let schema_file = std::fs::File::open(schema_path).unwrap();
+    let schema: Schema = serde_json::from_reader(schema_file).unwrap();
+
+    println!("{:?}", Into::<arrow::datatypes::Schema>::into(&schema));
+
+    let zip_path = std::path::PathBuf::from(std::env::args().nth(2).unwrap());
+    let zip_file = std::fs::File::open(zip_path).unwrap();
+
+    let xml_name = std::env::args().nth(3).unwrap();
+
+    let mut archive = zip::ZipArchive::new(zip_file).unwrap();
+    let file = archive.by_name(&xml_name).unwrap();
+    let buf_reader = std::io::BufReader::new(file);
+    let reader = ParserConfig::new()
+        .trim_whitespace(true)
+        .ignore_comments(true)
+        .create_reader(buf_reader);
+    // reader.config_mut().trim_text(true);
+    let columns = parse(&schema, reader);
+    // parse(&schema, reader)
+    println!("len = {}", columns.values().last().unwrap().len());
 }
