329298fedd1f0dc52ea29c8edb0c8d75f28d31c7
Leonhard Markert <curiousleo@users.noreply.github.com>
2025-11-12 20:45:41 +0800

    Write metadata

--- mastr-export/src/main.rs
+++ mastr-export/src/main.rs
@@ -3,7 +3,11 @@ mod schema;
 
 use anyhow::{Context, Result, anyhow};
 use argh::FromArgs;
-use parquet::{arrow::ArrowWriter, basic::ZstdLevel, file::properties::WriterProperties};
+use parquet::{
+    arrow::ArrowWriter,
+    basic::ZstdLevel,
+    file::{metadata::KeyValue, properties::WriterProperties},
+};
 use std::{ffi::OsStr, fs::File, io::BufWriter, path::PathBuf};
 
 use parser::XmlParser;
@@ -38,10 +42,6 @@ fn main() -> Result<()> {
         .map(|name| name.to_string())
         .collect::<Vec<_>>();
 
-    let parquet_writer_props = WriterProperties::builder()
-        .set_compression(parquet::basic::Compression::ZSTD(ZstdLevel::default()))
-        .build();
-
     for name in file_names {
         let (prefix, _) = name
             .split_once(&['_', '.'])
@@ -56,7 +56,7 @@ fn main() -> Result<()> {
         let zip_file = archive
             .by_name(&name)
             .map_err(|e| anyhow!("Failed to read XML file '{}' in archive: {}", name, e))?;
-        let buf_reader = std::io::BufReader::with_capacity(1024 * 1024, zip_file);
+        let buf_reader = std::io::BufReader::with_capacity(64 * 1024, zip_file);
         let xml_reader = XmlParser::create_reader(buf_reader);
         let record_batch = XmlParser::parse(schema, xml_reader)
             .context(format!("Failed to parse XML file {}", name))?;
@@ -67,11 +67,20 @@ fn main() -> Result<()> {
         let writer = BufWriter::new(File::create(
             args.parquet_dir.join(format!("{}.parquet", stem)),
         )?);
-        let mut arrow_writer = ArrowWriter::try_new(
-            writer,
-            record_batch.schema(),
-            Some(parquet_writer_props.clone()),
-        )?;
+
+        let key_value_metadata = record_batch
+            .schema()
+            .metadata()
+            .clone()
+            .into_iter()
+            .map(|(k, v)| KeyValue::new(k, v))
+            .collect::<Vec<_>>();
+        let parquet_writer_props = WriterProperties::builder()
+            .set_compression(parquet::basic::Compression::ZSTD(ZstdLevel::default()))
+            .set_key_value_metadata(Some(key_value_metadata))
+            .build();
+        let mut arrow_writer =
+            ArrowWriter::try_new(writer, record_batch.schema(), Some(parquet_writer_props))?;
         arrow_writer.write(&record_batch)?;
         arrow_writer.close()?;
         println!(
