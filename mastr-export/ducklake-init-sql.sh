c57fd02de27b266381f4c1affdb1cea3988d6b66
curiousleo <curiousleo@noreply.users.github.com>
2025-12-13 20:22:09 +0800

    Fix

--- mastr-export/ducklake-init-sql.sh
+++ mastr-export/ducklake-init-sql.sh
@@ -104,7 +104,7 @@ ATTACH 'ducklake:sqlite:catalog.sqlite3' AS $db_name (DATA_PATH 'tmp_always_empt
 DETACH $db_name;
 ATTACH 'catalog.sqlite3' AS catalog (TYPE sqlite);
 UPDATE catalog.ducklake_data_file
-   SET path = replace('$parquet_dir', '$data_url', '')
+   SET path = replace(path, '$parquet_dir', '$data_url')
  WHERE path LIKE '$parquet_dir/%';
 DETACH catalog;
 .
