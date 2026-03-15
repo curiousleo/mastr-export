2e97b3f3adfb6bd521d0a1cb4935b9b0fc27ffdf
curiousleo <curiousleo@users.noreply.github.com>
2026-03-15 21:19:30 +0800

    DROP DICTIONARY mastr.* is incorrect syntax

--- mastr-export.ts
+++ mastr-export.ts
@@ -464,8 +464,8 @@ async function initClickHouse(
   if (checkResp.trim() === "1") {
     // ClickHouse doesn't seem to support renaming databases atomically, so we
     // drop all dictionaries and views before renaming.
-    await ch(`DROP DICTIONARY ${db}.*`);
-    await ch(`DROP VIEW ${db}.*`);
+    await ch(`DROP VIEW ${db}.Einheiten`);
+    await ch(`DROP DICTIONARY ${db}.KatalogwerteDict`);
     await ch(`RENAME DATABASE ${db} TO ${old}`);
   }
   await ch(`RENAME DATABASE ${staging} TO ${db}`);
